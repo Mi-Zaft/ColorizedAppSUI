5486adc2d6c05e06a588dbb69150b7c3bcdbd8a0
MiZaft <mi.zaft1@gmail.com>
2023-11-14 21:46:46 +0300

    Initial Commit

---
+++ ColorizedAppSUI/ColorizedAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedAppSUIApp.swift
+//  ColorizedAppSUI
+//
+//  Created by Максим Евграфов on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
